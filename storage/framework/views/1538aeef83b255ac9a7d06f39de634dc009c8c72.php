a25303a8d013d20f7e481c0321d62457b83948f6
premkumar@mazenetsolution.com <devops@mazenetsolution.com>
2021-01-09 11:40:26 +0530

    new

--- storage/framework/views/1538aeef83b255ac9a7d06f39de634dc009c8c72.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<main class="page-content">
-
-<div class="col-12 body-sec"></div>
-<?php $__env->stopSection(); ?>
-
-
-<?php echo $__env->make('admin.layout.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ns_pollachi\resources\views/admin/master/empty.blade.php ENDPATH**/ ?>
--- storage/framework/views/6b2da34b7cca6390ed78f3e0503a3ec13ac92246.php
+++
@@ -1,5 +0,0 @@
-<?php echo $__env->make('admin.layout.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php echo $__env->yieldContent('content'); ?>
-
-<?php echo $__env->make('admin.layout.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ns_pollachi\resources\views/admin/layout/app.blade.php ENDPATH**/ ?>
